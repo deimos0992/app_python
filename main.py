8fde06ce5c778b4f2e1a6f1e1fc3dbce2b8c72a5
deimos <luigi.benvenuto01@gmail.com>
2021-01-07 14:25:37 +0100

    initial app

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from kivymd.app import MDApp
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.title = "Hello Kivy"
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.prymary_palette = "Red"
+
+
+MainApp().run()
